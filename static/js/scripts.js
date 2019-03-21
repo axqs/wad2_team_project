33f0302ba5d6075e80826a23d163a9bff313ee9b
Eve O'Hagan <2251923o@student.gla.ac.uk>
2019-03-21 10:44:50 +0000

    js part fixed

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -4,27 +4,3 @@ function create_review(){
 	console.log("Review form has been sumitted!");
 
 }
-
-function hide_show_one(){
-    		$(document).ready(function(){
-			$(".btn1").click(function(){
-				$("p").hide();
-			});
-			$(".btn2").click(function(){
-				$("p").show();
-			});
-		});
-    
-}
-
-function hide_show_two(){
-    		$(document).ready(function(){
-			$(".btn3").click(function(){
-				$("p1").hide();
-			});
-			$(".btn4").click(function(){
-				$("p1").show();
-			});
-		});
-    
-}
